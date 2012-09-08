77de4ca8710de8d2890d17842999c0f30bac6646
Gregor Tudan <G.Tudan@gmx.de>
2012-09-08 20:07:05 +0200

    Initial Import

---
+++ src/de/tudan/otrsclient/SoapArrayFactory.java
@@ -0,0 +1,39 @@
+package de.tudan.otrsclient;
+
+import java.util.Collection;
+
+import javax.xml.soap.SOAPElement;
+import javax.xml.soap.SOAPException;
+import javax.xml.soap.SOAPFactory;
+
+import org.w3c.dom.DOMException;
+
+/**
+ * Utility to create SoapEncoded Arrays from Collections
+ * 
+ * @author Gregor Tudan
+ */
+public class SoapArrayFactory {
+
+	protected <T> SOAPElement createSoapArray(String name, Collection<T> values)
+			throws DOMException, SOAPException {
+		SOAPElement element = SOAPFactory.newInstance().createElement(name);
+
+		// Check the type of the array and default to string if empty
+		String type;
+		if (values.size() != 0) {
+			type = XSDTypeConverter.simpleTypeForObject(values.iterator().next());
+		} else {
+			type = "xsi:string";
+		}
+
+		element.setAttribute("soapenc:arrayType", type + "[" + values.size()+ "]");
+		element.setAttribute("xsi:type", "soapenc:Array");
+
+		for (T s : values) {
+			element.addChildElement("item").addTextNode(s.toString()).setAttribute("xsi:type", type);
+		}
+
+		return element;
+	}
+}
